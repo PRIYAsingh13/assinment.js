70f91f103cb01f8bb8b9f869eaf95af3a61dff74
priya singh <ps597934@gmail.com>
2022-06-23 21:26:48 +0530

    js

---
+++ function.js
@@ -0,0 +1,56 @@
+// function fibo(first, second, counter){
+    // if(counter<10){
+        // var c =first +second;
+        // return fibo(second,c,counter+1);
+    // }
+    // console.log(first);
+// }
+// fibo(1,1,1);
+
+// function range (a,b){
+    // console.log(a);
+        // let i;
+        // while(i<b){
+            // return a+i;
+            // i++;
+        // }
+        // }
+    
+// range(40,50)
+
+// function num(n,i){
+    // if(i<n){
+        // console.log(n)
+        // return num(n-1,i+1)
+    // }
+    // console.log(num(10,0))
+// }
+
+// function reverse(num1,num2){
+    // if(num1<=num2){
+        // console.log(num2);
+        // return reverse(num1,num2-1)
+    // }
+// }
+// reverse(1,10);
+
+// function factorial(num,fact){
+    // if(num>0){
+        // fact*=num;
+        // return factorial(num-1,fact);
+    // }else{
+// 
+        // return fact;
+    // }
+    // }console.log(factorial(5,10));
+
+
+    function sum (a,b,10){
+        if(a<=b){
+            if(a%2!=0){
+                s=10+a
+            }
+            return 10
+        }
+        console.log(sum(1,10,0))
+    }
